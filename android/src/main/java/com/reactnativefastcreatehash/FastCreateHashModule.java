58988b89d8ebac4dd6dc1e04389bf03dfdcf30b8
Maximilian Fellner <mail@mfellner.eu>
2022-04-17 00:33:46 +0200

    feat(android): add sha support for android

--- android/src/main/java/com/reactnativefastcreatehash/FastCreateHashModule.java
+++ android/src/main/java/com/reactnativefastcreatehash/FastCreateHashModule.java
@@ -1,5 +1,7 @@
 package com.reactnativefastcreatehash;
 
+import android.util.Base64;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
@@ -7,6 +9,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @ReactModule(name = FastCreateHashModule.NAME)
 public class FastCreateHashModule extends ReactContextBaseJavaModule {
@@ -22,13 +27,23 @@ public class FastCreateHashModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
-
     // Example method
     // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void createHash(String data64, String algorithm, Promise promise) {
+        try {
+            String shaNumber = algorithm.substring(3);
+            MessageDigest digest = MessageDigest.getInstance("SHA-" + shaNumber);
+            byte[] in = Base64.decode(data64, Base64.DEFAULT);
+            digest.update(in);
+            byte[] out = digest.digest();
+            String base64 = Base64.encodeToString(out, Base64.NO_WRAP);
+            promise.resolve(base64);
+        } catch (IndexOutOfBoundsException | NoSuchAlgorithmException e) {
+            promise.reject("unknown_algorithm", "hashing algorithm not supported", e);
+        } catch (IllegalArgumentException e) {
+            promise.reject("bad_data", "data cannot be parsed", e);
+        }
+        promise.resolve("abc");
     }
-
-    public static native int nativeMultiply(int a, int b);
 }
